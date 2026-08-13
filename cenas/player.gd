2868be3c2854a6fe3f5ef174de953c2bb6ac47df
Unifor Lab <email@example.com>
2026-08-13 20:22:46 -0300

    Galinha ANDAAAAAAA

--- cenas/player.gd
+++ cenas/player.gd
@@ -3,18 +3,42 @@ extends Area2D
 
 @export var speed: float = 100.0 #100 pixels por segundo
 var posicao_inicial: Vector2 = Vector2(640,690)
+var screen_size: Vector2
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	screen_size = get_viewport().size
 	position = posicao_inicial
 	
-	
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var vetor_velocidade = Vector2.ZERO
+	
+	if(Input.is_action_pressed("ui_up")):
+		vetor_velocidade.y -=1
+	
+	if(Input.is_action_pressed("ui_down")):
+		vetor_velocidade.y +=1
+	
+	if(vetor_velocidade !=Vector2.ZERO):
+		vetor_velocidade = vetor_velocidade.normalized()*speed
+	
+	position +=vetor_velocidade*delta
+	position.y = clamp(position.y,0.0,screen_size.y)
+	
+	if(vetor_velocidade.y > 0):
+		$Animacao.play("baixo")
 
+	elif(vetor_velocidade.y<0):
+		$Animacao.play("cima")
 
+	else:
+		$Animacao.stop()
+		
+#FALAR O NOME DO ARTHUR
 func somaDoida(a:float,b:float) -> float:
 		return a+b
