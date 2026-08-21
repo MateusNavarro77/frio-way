3a7d7a720097e1d40981df6847ec4db812a6e9dc
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2026-08-21 00:14:56 -0300

    popopo da galinha

--- cenas/player.gd
+++ cenas/player.gd
@@ -5,6 +5,14 @@ signal pontua
 var posicao_inicial: Vector2 = Vector2(640,690)
 var screen_size: Vector2
 
+@onready var audio1 : AudioStreamPlayer2D= $AudioPopo1
+@onready var audio2 : AudioStreamPlayer2D= $AudioPopo2
+@onready var audio3: AudioStreamPlayer2D = $AudioPopo3
+@onready var audios : Array[AudioStreamPlayer2D]= [audio1,audio2,audio3]
+const TEMPO_ENTRE_POPOS: float = 10
+
+var tempo_passado=0
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -17,6 +25,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var vetor_velocidade = Vector2.ZERO
+	
+	
+	var passou_tempo = checar_tempo(delta)
+	if(passou_tempo):
+		tocar_popo_da_galinha()
+	
 	
 	if(Input.is_action_pressed("ui_up")):
 		vetor_velocidade.y -=1
@@ -50,10 +64,24 @@ func _process(delta: float) -> void:
 func somaDoida(a:float,b:float) -> float:
 		return a+b
 
-
+func checar_tempo(delta:float) -> bool:
+	tempo_passado += delta
+	if(tempo_passado>=TEMPO_ENTRE_POPOS):
+		tempo_passado = 0
+		return true
+	return false
+	
+	
 func _on_body_entered(body: Node2D) -> void:
 	if (body.name == "LinhaChegada"):
 		emit_signal("pontua")
 		return
 	$Audio.play()
 	position = posicao_inicial
+	
+func tocar_popo_da_galinha():
+	var indice_escolhido = randi_range(0,audios.size()-1)
+	var popo = audios[indice_escolhido]
+	popo.play()
+
+	pass
